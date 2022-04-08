7818e64a2785377f164db21c9e3be045378d6fd2
Irasema0915 <angelaruiz.0915@gmail.com>
2022-04-08 01:17:26 -0500

    corrigiendo errores fetch

--- sw.js
+++ sw.js
@@ -67,14 +67,13 @@ self.addEventListener('activate', event => {
 
 */
 self.addEventListener('fetch', event => {
-    const respuesta = caches.match(event.request).then( res => {
-        if(res){return res;}
-        else{
-            return fetch(e.request).then(newRes => {
-                //Agregar en el directorio /js un archivo llamdado sw-acces.js
-                //y programar la funcion actualizaCacheDinamico, para tener mas limpio el proyecto.
+    const respuesta = caches.match(event.request).then(res => {
+        if (res) { return res; } else {
+            return fetch(event.request).then(newRes => {
                 return actualizaCacheDinamico(DYNAMIC_CACHE, event.request, newRes);
             });
         }
     });
+
+    event.respondWith(respuesta);
 });
