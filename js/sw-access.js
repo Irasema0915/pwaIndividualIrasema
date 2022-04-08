811a4fc7951c7a6394e7da2e4e3ce6ad02fec917
Irasema0915 <angelaruiz.0915@gmail.com>
2022-04-08 01:23:26 -0500

    corrigiendo errores fetch 1.2

--- js/sw-access.js
+++ js/sw-access.js
@@ -2,15 +2,15 @@
 /*
     *   1. Hacer una función para actualizar el cache dinamico cuando algo no se encuentre en el APP_SHELL
 */
-
-function actualizaCacheDinamico(dynamicCache, request, response){
-    if(response.ok){
+function actualizaCacheDinamico(dynamicCache, request, response) {
+    if (response.ok) {
         //la respuesta tiene data y se debe almacaenar en cache
         return caches.open(dynamicCache).then(cache => {
-            cache.put(request,response.clone() );
+            cache.put(request, response.clone());
             return cache.clone();
         });
-    }else{//si no viene nda en la respuesta
+    } else { //si no viene nada en la respuesta
         return response;
     }
 }
+
